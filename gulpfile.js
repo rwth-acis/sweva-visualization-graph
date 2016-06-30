edff24b3a5566cd7303c002ec5e1fc693cea3d70
istvank <koren@dbis.rwth-aachen.de>
2016-06-30 16:58:12 +0200

    Added new 'build' task in gulp to add a 'min' for minified files.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var vulcanize = require('gulp-vulcanize');
 var replace = require('gulp-replace');
 var minifyHTML = require('gulp-minify-html');
 var minifyInline = require('gulp-minify-inline');
+var rename = require('gulp-rename');
+var debug = require('gulp-debug');
 
 var FILE = 'sweva-visualization-graph.html'
 gulp.task('pack', function () {
@@ -15,15 +17,17 @@ gulp.task('pack', function () {
         .pipe(vulcanize({
             stripComments: true,
             inlineCss: true,
-            inlineScripts: true
+            inlineScripts: false
         }))
-       
         .pipe(minifyInline())
         .pipe(minifyHTML({
             quotes: true,
             empty: true,
             spare: true
-        }))        
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('dist'))
 });
 
@@ -40,3 +44,5 @@ gulp.task('packdev', function () {
         }))        
         .pipe(gulp.dest('dist'))
 });
+
+gulp.task('build', ['pack', 'packdev']);
